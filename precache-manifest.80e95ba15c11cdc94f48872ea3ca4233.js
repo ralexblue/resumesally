b0a3beb91e5f1ad6e452d41b4ec749c3e731475b
Alex <robert.perez6@g.austincc.edu>
2020-01-01 13:53:45 -0600

    Updates

---
+++ precache-manifest.80e95ba15c11cdc94f48872ea3ca4233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad84791e22cef0ce69f2ad6417c01a04",
+    "url": "/resumesally/index.html"
+  },
+  {
+    "revision": "53c65dd4dc2afa2cad39",
+    "url": "/resumesally/static/css/main.e90d63dc.chunk.css"
+  },
+  {
+    "revision": "ae3261ba0166b7c80e1a",
+    "url": "/resumesally/static/js/2.ba82a890.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/resumesally/static/js/2.ba82a890.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53c65dd4dc2afa2cad39",
+    "url": "/resumesally/static/js/main.35dcb7e7.chunk.js"
+  },
+  {
+    "revision": "c30a163c4fec1f8473eb",
+    "url": "/resumesally/static/js/runtime-main.2705a888.js"
+  }
+]);
